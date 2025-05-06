145485102e0460a0296c48f9d73fcdb74818c8f7
Denio1337 <danil.kolmogorov@yandex.ru>
2025-05-06 20:17:50 +0700

    Добавлен ansible.cfg + изменена структура

--- ansible/entrypoint.sh
+++ ansible/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+# Генерация и регистрация ключа на управляемых узлах
 ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -N ''
 sshpass -p "$SSH_PASSWORD" ssh-copy-id -o StrictHostKeyChecking=no root@managed1
 sshpass -p "$SSH_PASSWORD" ssh-copy-id -o StrictHostKeyChecking=no root@managed2
