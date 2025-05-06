8d4a4227d5e72a06823e5ca83efea2167d98e48f
Denio1337 <danil.kolmogorov@yandex.ru>
2025-05-06 21:02:34 +0700

    Добавлен пример плейбука + изменение структуры

--- ansible/entrypoint.sh
+++ ansible/entrypoint.sh
@@ -6,4 +6,7 @@ ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -N ''
 sshpass -p "$SSH_PASSWORD" ssh-copy-id -o StrictHostKeyChecking=no root@managed1
 sshpass -p "$SSH_PASSWORD" ssh-copy-id -o StrictHostKeyChecking=no root@managed2
 
+# Копирование конфига Ansible по дефолтному пути
+cp cfg /etc/ansible/ansible.cfg
+
 exec bash
