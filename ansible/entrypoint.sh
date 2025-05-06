4057668ef4b5d6aefd009b83d7dbd5347520ca5b
Denio1337 <danil.kolmogorov@yandex.ru>
2025-05-06 14:54:42 +0700

    Добавлен файл с переменными окружения
    В файл с переменными окружения вынесен пароль от root-пользователя каждого из управляемых узлов. Используется для первичного подключения с паролем по SSH с управляющего узла с целью регистрации ключа и последующего беспарольного доступа.

--- ansible/entrypoint.sh
+++ ansible/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -N ''
-sshpass -p 'password' ssh-copy-id -o StrictHostKeyChecking=no root@managed1
-sshpass -p 'password' ssh-copy-id -o StrictHostKeyChecking=no root@managed2
+sshpass -p "$SSH_PASSWORD" ssh-copy-id -o StrictHostKeyChecking=no root@managed1
+sshpass -p "$SSH_PASSWORD" ssh-copy-id -o StrictHostKeyChecking=no root@managed2
 
 exec bash
